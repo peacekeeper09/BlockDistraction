033a377e40ed0d6ed2894e6ad90e9e3bed4558b0
peacekeeper09 <80287027+peacekeeper09@users.noreply.github.com>
2023-10-30 02:04:09 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,31 @@
+const config = {
+  blockRules: [
+    { selector: '.ad, .advertisement, .textwidget, .custom-html-widget', action: 'hide' },
 
+  ],
+  classSelectors: [
+    '.ad, .ad-container, .ad-banner, .adsbygoogle, .taboola, .outbrain',
+  ],
+};
+
+function applyRules(rules) {
+  rules.forEach(rule => {
+    const elements = document.querySelectorAll(rule.selector);
+    elements.forEach(element => {
+      element.style.display = rule.action === 'hide' ? 'none' : '';
+    });
+  });
+}
+
+function applyClassNameBasedBlocking(classSelectors) {
+  const selectorString = classSelectors.join(', ');
+  const adElements = document.querySelectorAll(selectorString);
+  adElements.forEach(adElement => {
+    adElement.style.display = 'none';
+  });
+}
+
+applyRules(config.blockRules);
+
+
+applyClassNameBasedBlocking(config.classSelectors);
